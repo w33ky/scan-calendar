a1e20838012a6f32c196b36ff677fca6e53693d8
Stefan Weckend <stefan.weckend@gmail.com>
2016-12-19 16:35:53 +0100

    changing Routes

--- src/AppBundle/Controller/CalendarController.php
+++ src/AppBundle/Controller/CalendarController.php
@@ -41,7 +41,8 @@ class CalendarController extends Controller
     }
 
     /**
-     * @Route("/api/list/{id}", name="list_calendar_rest")
+     * @Route("/api/appointment/{id}", name="list_calendar_rest")
+     * @Method("GET")
      */
     public function listRestAction($id)
     {
@@ -52,6 +53,7 @@ class CalendarController extends Controller
 
     /**
      * @Route("/api/upload", name="upload_rest")
+     * @Method("POST")
      */
     public function uploadRestAction(Request $request)
     {
@@ -95,7 +97,8 @@ class CalendarController extends Controller
     }
 
     /**
-     * @Route("/api/list", name="list_all_calendar_rest")
+     * @Route("/api/appointment", name="list_all_calendar_rest")
+     * @Method("GET")
      */
     public function listAllRestAction()
     {
@@ -105,7 +108,8 @@ class CalendarController extends Controller
     }
 
     /**
-     * @Route("/api/delete/{id}", name="delete_calendar")
+     * @Route("/api/appointment/{id}", name="delete_calendar")
+     * @Method("DELETE")
      */
     public function deleteRestAction($id)
     {
@@ -113,6 +117,7 @@ class CalendarController extends Controller
         $calendar = $em->getRepository('AppBundle:Calendar')->find($id);
 
         unlink('images/' . $calendar->getId() . '.png');
+        unlink('images/' . $calendar->getId() . '_restya' . '.png');
 
         $em->remove($calendar);
         $em->flush();
@@ -165,6 +170,7 @@ class CalendarController extends Controller
         $calendar = $em->getRepository('AppBundle:Calendar')->find($id);
 
         unlink('images/' . $calendar->getId() . '.png');
+        unlink('images/' . $calendar->getId() . '_restya' . '.png');
 
         $em->remove($calendar);
         $em->flush();
@@ -175,6 +181,63 @@ class CalendarController extends Controller
         );
 
         return $this->redirectToRoute('list_calendar');
+    }
+
+    private function thumbnail_box($img, $box_w, $box_h) {
+        //create the image, of the required size
+        $new = imagecreatetruecolor($box_w, $box_h);
+        if($new === false) {
+            //creation failed -- probably not enough memory
+            return null;
+        }
+
+
+        //Fill the image with a light grey color
+        //(this will be visible in the padding around the image,
+        //if the aspect ratios of the image and the thumbnail do not match)
+        //Replace this with any color you want, or comment it out for black.
+        //I used grey for testing =)
+        $fill = imagecolorallocate($new, 255, 255, 255);
+        imagefill($new, 0, 0, $fill);
+
+        //compute resize ratio
+        $hratio = $box_h / imagesy($img);
+        $wratio = $box_w / imagesx($img);
+        $ratio = min($hratio, $wratio);
+
+        //if the source is smaller than the thumbnail size,
+        //don't resize -- add a margin instead
+        //(that is, dont magnify images)
+        if($ratio > 1.0)
+            $ratio = 1.0;
+
+        //compute sizes
+        $sy = floor(imagesy($img) * $ratio);
+        $sx = floor(imagesx($img) * $ratio);
+
+        //compute margins
+        //Using these margins centers the image in the thumbnail.
+        //If you always want the image to the top left,
+        //set both of these to 0
+        $m_y = floor(($box_h - $sy) / 2);
+        $m_x = floor(($box_w - $sx) / 2);
+
+        //Copy the image data, and resample
+        //
+        //If you want a fast and ugly thumbnail,
+        //replace imagecopyresampled with imagecopyresized
+        if(!imagecopyresampled($new, $img,
+            $m_x, $m_y, //dest x, y (margins)
+            0, 0, //src x, y (0,0 means top left)
+            $sx, $sy,//dest w, h (resample to this size (computed above)
+            imagesx($img), imagesy($img)) //src w, h (the full size of the original)
+        ) {
+            //copy failed
+            imagedestroy($new);
+            return null;
+        }
+        //copy successful
+        return $new;
     }
 
     /**
@@ -630,6 +693,10 @@ class CalendarController extends Controller
 
         // Bild schreiben
         imagepng($bild, 'images/' . $pathToSnippet . '.png');
+
+        $ratio = 150/202;
+        $restya_picture = $this->thumbnail_box($bild, $width, floor($width * $ratio));
+        imagepng($restya_picture, 'images/' . $pathToSnippet . '_restya' . '.png');
     }
 
     /**
